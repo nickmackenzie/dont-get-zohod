f27f02fe89595cdf49ea503f3c99fa13b7d07fe1
Doug <dmick92@gmail.com>
2023-05-12 14:13:17 -0400

    Added global copy + fixed project link

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,11 +4,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Don't Get ZOHO'D</span>,
   project: {
-    link: 'hhttps://github.com/nickmackenzie/dont-get-zohod',
+    link: 'https://github.com/nickmackenzie/dont-get-zohod',
   },
 
   docsRepositoryBase: 'https://github.com/nickmackenzie/dont-get-zohod',
-
 }
 
 export default config
