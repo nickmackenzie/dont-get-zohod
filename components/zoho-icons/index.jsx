3077fe69f0a309deed1d62b81e4a2c1b3298203d
dmick92 <dmick92@gmail.com>
2024-01-13 11:06:06 -0500

    Added zoho icons components + restructured components folder

---
+++ components/zoho-icons/index.jsx
@@ -0,0 +1,151 @@
+import styles from "./zoho-icons.module.css";
+
+export const TestIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-360px -40px" }}
+    />
+  );
+};
+
+export const AssistIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-720px 0" }} />
+  );
+};
+
+export const BooksIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "0 -40px" }} />
+  );
+};
+
+export const CampaignsIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-440px 0" }} />
+  );
+};
+
+export const CatalystIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-400px -80px" }}
+    />
+  );
+};
+
+export const CommerceIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-280px -80px" }}
+    />
+  );
+};
+
+export const CrmIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-320px 0" }} />
+  );
+};
+
+export const CreatorIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-720px -40px" }}
+    />
+  );
+};
+
+export const DelugeIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-720px -120px" }}
+    />
+  );
+};
+
+export const DeskIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-680px 0" }} />
+  );
+};
+
+export const FormsIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-560px 0" }} />
+  );
+};
+
+export const IventoryIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-40px -40px" }}
+    />
+  );
+};
+
+export const InvoiceIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-160px -40px" }}
+    />
+  );
+};
+
+export const MailIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-360px -40px" }}
+    />
+  );
+};
+
+export const MeetingsIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-760px 0" }} />
+  );
+};
+
+export const PeopleIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-280px -40px" }}
+    />
+  );
+};
+
+export const RecruitIcon = () => {
+  return (
+    <span
+      className={styles.zicon}
+      style={{ backgroundPosition: "-240px -40px" }}
+    />
+  );
+};
+
+export const SalesIqIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-360px 0" }} />
+  );
+};
+
+export const SignIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-640px 0" }} />
+  );
+};
+
+export const SitesIcon = () => {
+  return (
+    <span className={styles.zicon} style={{ backgroundPosition: "-600px 0" }} />
+  );
+};
