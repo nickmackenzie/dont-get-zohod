3cf358435136fe4dd1b2532219349e09beb67b5a
Nick Mackenzie <54223351+nickmackenzie@users.noreply.github.com>
2024-01-18 19:36:45 +0000

    add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://dont-get-zohod.vercel.app/",
+    generateRobotsTxt: true,
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 import nextra from "nextra";
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-  staticImage: true,
-  latex: true,
-  defaultShowCopyCode: true,
-
+    theme: "nextra-theme-docs",
+    themeConfig: "./theme.config.tsx",
+    staticImage: true,
+    latex: true,
+    defaultShowCopyCode: true,
 });
 
 export default withNextra({
-  reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
+    reactStrictMode: true,
+    eslint: { ignoreDuringBuilds: true },
+    i18n: {
+        locales: ["en"], // List all locales you want to support
+        defaultLocale: "en", // Set your default locale
+    },
 });
